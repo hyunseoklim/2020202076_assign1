e40070e7fac38711919fe2a19abad3640a31ca7b
unknown <andyy7919@gmail.com>
2024-11-01 17:43:04 +0900

    Version upgrade

--- lib.test.js
+++ lib.test.js
@@ -8,3 +8,27 @@ test("avg([3, 5, 7]) should be 5", () => {
 test("avg([-5, 5]) should be 0", () => {
     expect(lib.avg([-5, 5])).toBe(0);
 });
+
+test("prime(2) should be true", () => {
+    expect(lib.prime(2)).toBe(true);
+});
+
+test("prime(4) should be false", () => {
+    expect(lib.prime(4)).toBe(false);
+});
+
+test("prime(13) should be true", () => {
+    expect(lib.prime(13)).toBe(true);
+});
+
+test("factorial(5) should be 120", () => {
+    expect(lib.factorial(5)).toBe(120);
+});
+
+test("factorial(0) should be 1", () => {
+    expect(lib.factorial(0)).toBe(1);
+});
+
+test("factorial(1) should be 1", () => {
+    expect(lib.factorial(1)).toBe(1);
+});
